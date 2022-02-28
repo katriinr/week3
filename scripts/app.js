6c10aa633afddaf4b276df62b5964db647e906fb
katriin r <katriin.ryytsalu@gmail.com>
2022-02-28 17:11:42 +0200

    jaagup aita hädast välja, ma ei taha olla mäger

---
+++ scripts/app.js
@@ -0,0 +1,25 @@
+
+//select movie poster original images
+let moviePoster=document.querySelector('.movie-poster');
+
+//select modal window layer
+let modal= document.querySelector('#myModal');
+//select close button
+let closeBtn= document.querySelector('.closeBtn');
+//select modal window image
+let modalImage= document.querySelector('.movie-poster-modal');
+moviePoster.addEventListener('click',()=> {
+    modal.style.display='block'
+    modalImage.src= movie-poster.src;
+});
+closeBtn.addEventListener('click',()=>{
+    modal.style.display='none'
+
+})
+window.addEventListener('click', (e)=>{
+    if(e.target === modal){
+        modal.style.display = 'none';
+
+    }
+})
+
