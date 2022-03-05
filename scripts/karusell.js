d77d1d1ac2313b47b25b47679d417638457f963b
katriin r <katriin.ryytsalu@gmail.com>
2022-03-05 20:42:18 +0200

    add carousel

--- scripts/karusell.js
+++ scripts/karusell.js
@@ -10,21 +10,22 @@ nextButton.addEventListener('click', () => {
         slide.classList.add('carousel__item--hidden');
     }
 
-    if(slidePosition === slidesTotal-1) {
+    if(slidePosition === slidesTotal - 1) {
         slidePosition = 0;
     }
     else {slidePosition++;
     }
    slides [slidePosition].classList.add('carousel__item--visible');
  });
+
 prevButton.addEventListener('click', () => {
     for (let slide of slides) {
         slide.classList.remove('carousel__item--visible');
         slide.classList.add('carousel__item--hidden');
     }
 
-    if(slidePosition === slidesTotal-1) {
-        slidePosition = 0;
+    if (slidePosition === 0) {
+        slidePosition = slidesTotal-1;
 }
  else{
     slidePosition--;
